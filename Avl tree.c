7e59cf21de78867e8631cc8b6e0861cba1829635
Srilakshmisai2005 <121482153+Srilakshmisai2005@users.noreply.github.com>
2023-08-07 20:24:51 +0530

    Add files via upload

---
+++ Avl tree.c
@@ -0,0 +1,65 @@
+      free(temp);
+    } else {
+      struct Node *temp = minValueNode(root->right);
+
+      root->key = temp->key;
+
+      root->right = deleteNode(root->right, temp->key);
+    }
+  }
+
+  if (root == NULL)
+    return root;
+
+  root->height = 1 + max(height(root->left),
+               height(root->right));
+
+  int balance = getBalance(root);
+  if (balance > 1 && getBalance(root->left) >= 0)
+    return rightRotate(root);
+
+  if (balance > 1 && getBalance(root->left) < 0) {
+    root->left = leftRotate(root->left);
+    return rightRotate(root);
+  }
+
+  if (balance < -1 && getBalance(root->right) <= 0)
+    return leftRotate(root);
+
+  if (balance < -1 && getBalance(root->right) > 0) {
+    root->right = rightRotate(root->right);
+    return leftRotate(root);
+  }
+
+  return root;
+}
+
+
+void printPreOrder(struct Node *root) {
+  if (root != NULL) {
+    printf("%d ", root->key);
+    printPreOrder(root->left);
+    printPreOrder(root->right);
+  }
+}
+
+int main() {
+  struct Node *root = NULL;
+
+  root = insertNode(root, 2);
+  root = insertNode(root, 1);
+  root = insertNode(root, 7);
+  root = insertNode(root, 4);
+  root = insertNode(root, 5);
+  root = insertNode(root, 3);
+  root = insertNode(root, 8);
+
+  printPreOrder(root);
+
+  root = deleteNode(root, 3);
+
+  printf("\nAfter deletion: ");
+  printPreOrder(root);
+
+  return 0;
+}
